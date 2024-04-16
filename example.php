a1f2704ae2668ab75e39dc6109adf639803fc178
Rizwan <hi@iamrizwan.me>
2024-04-16 21:01:24 +0500

    updated example.php

--- example.php
+++ example.php
@@ -1,10 +1,14 @@
 <?php
+error_reporting(0);
 
-require_once 'EmailVerifier.php';
+use rizwan_47\EmailVerifier\EmailVerifier;
+
+// Load dependencies
+require __DIR__ . '/vendor/autoload.php';
+
+
 $email = 'contact@gbober.com';
 $verifier = new EmailVerifier($email);
 $response = $verifier->verify();
+
 print_r($response);
-
-
-?>
